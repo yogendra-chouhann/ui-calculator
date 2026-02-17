7911e1d31e2af7cb3a9656e33984cbbc60a51e9b
Yogendra Chouhan <yogii@Yogendras-MacBook-Air.local>
2026-02-17 13:57:57 +0530

    Day 2 – Added UX polish and button interactions

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,31 +1,66 @@
 console.log(" UI Calculator")
+
+function add(n1,n2){
+         return n1+n2
+    }
+
 function calculate(type) {
     n1 = Number(document.getElementById('number1').value)
     n2 = Number(document.getElementById('number2').value)
+
     let result
 
+    function add(n1,n2){
+         return n1+n2
+    }
     if (type=== "add") {
-       result = n1+n2
+       result = add(n1,n2)
+    }
+    function subtract(n1,n2){
+        return  n1-n2
     }
      if (type=== "subtract") {
-       result =   n1-n2
+       result =   subtract(n1,n2)
+    }
+    function multiplication(n1,n2){
+        return n1*n2
     }
      if (type=== "multiplication") {
-        result =  n1*n2
+        result = multiplication(n1,n2)
+    }
+    function divide(n1,n2){
+        if (n2===0) {
+            return 'can not divide by zero'
+        }
+        else {
+            return n1/n2
+        }
     }
      if (type=== "divide") {
-        result = n2===0? 'cannot divide by zero ' : n1/n2
+        result = divide(n1,n2)
+    }
+    function percentage(n1,n2){
+        return ((n1-n2)/((n1+n2)/2))*100
     }
      if (type=== "percentage") {
-        result =  ((n1-n2)/((n1+n2)/2))*100
+        result =  percentage(n1,n2)
+    }
+    function power(n1,n2){
+        return  n1**n2
     }
     if (type=== "power") {
-        result =  n1**n2
+        result = power(n1,n2)
     }
     document.getElementById("result").innerText = ('Result : ' +result)
 }
+
+
 function clearAll(){
     document.getElementById("result").innerText= " Calculator Reset  "
     document.getElementById('number1').value = ""
     document.getElementById('number2').value = ""
 }
+resultBox.style.opacity = 0;
+setTimeout(()=>{
+    resultBox.style.opacity = 1;
+},100);
